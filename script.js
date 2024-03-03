d0565068dbee5fb1f1eee4bbf3bd53546d5ea13f
Artur Nikytenko <arturnikytenko@gmail.com>
2024-03-04 00:50:40 +0100

    Creating usernames

--- script.js
+++ script.js
@@ -75,6 +75,17 @@ const displayMovements = function (movements) {
   });
 };
 displayMovements(account1.movements);
+
+const createUsernames = function (accs) {
+  accs.forEach(function (acc) {
+    acc.username = acc.owner
+      .toLowerCase()
+      .split(' ')
+      .map(word => word[0])
+      .join('');
+  });
+};
+createUsernames(accounts);
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
