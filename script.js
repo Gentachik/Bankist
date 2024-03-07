1a38e8116a382ffeaf346b59c4a4021af74d0bdf
Artur Nikytenko <arturnikytenko@gmail.com>
2024-03-07 23:40:08 +0100

    implementing sort operation

--- script.js
+++ script.js
@@ -61,10 +61,16 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
-const displayMovements = function (movements) {
+const displayMovements = function (movements, sort = false) {
   containerMovements.innerHTML = '';
 
-  movements.forEach(function (mov, i) {
+  const movs = sort
+    ? movements.slice().sort((a, b) => {
+        a - b;
+      })
+    : movements;
+
+  movs.forEach(function (mov, i) {
     const type = mov > 0 ? 'deposit' : 'withdrawal';
     const html = `<div class="movements__row">
     <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
@@ -186,6 +192,13 @@ btnClose.addEventListener('click', function (e) {
   }
 
   inputClosePin.value = inputCloseUsername.value = '';
+});
+
+let sorted = false;
+btnSort.addEventListener('clock', function (e) {
+  e.defaultPrevented();
+  displayMovements(currentAccount.movements, !sorted);
+  sorted = !sorted;
 });
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
